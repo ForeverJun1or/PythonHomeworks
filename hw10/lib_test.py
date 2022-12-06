015bec981c87968f583a82bbca48e2699784a4ff
Kyrylo Divisienko <racket952@gmail.com>
2022-12-06 08:50:19 +0200

    update 10's hw

--- hw10/lib_test.py
+++ hw10/lib_test.py
@@ -8,13 +8,14 @@ def test_exponent_TypeError():
     with pytest.raises(TypeError):
         assert exponent_multiplication(5, "2", exp=2)
 
-def test_exponent_when_exponent():
-        assert exponent_multiplication(5, -2, exp=-0.5)
+def test_exponent_TypeError():
+    with pytest.raises(ValueError):
+        assert exponent_multiplication(2, 3, exp=-2)
 
 def test_exponent_returned_value_is_int():
-        assert type(exponent_multiplication(5, -2, exp=0)) == int, 'returned value is not int'
+        assert type(exponent_multiplication(5, -2, exp=2)) == int, 'returned value is not int'
 
 def test_exponent_returned_value_is_float():
-    assert type(exponent_multiplication(5.2, -2, exp=0)) == float, 'returned value is not float'
+    assert type(exponent_multiplication(5.2, -2, exp=2)) == float, 'returned value is not float'
 
 # TODO: аргументировать почему нету декоратора, доделать тесты,
--- hw10/library.py
+++ hw10/library.py
@@ -1,3 +1,5 @@
 def exponent_multiplication(first_val: int | float, second_val: int | float, /, *, exp: int | float) -> int | float:
+    if exp < 1:
+        raise ValueError
     result = (first_val * second_val) ** exp
     return result
