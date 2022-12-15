04e5aa3e81284f4d79e7c1b4f8fd5dc062130461
Kyrylo Divisienko <racket952@gmail.com>
2022-12-15 11:59:38 +0200

    update hw13

--- hw13/homework13.py
+++ hw13/homework13.py
@@ -3,19 +3,29 @@ from Figures import Point, Triangle
 
 def create_triangle():
     first_point = Point(0, 0)
-    print(f'Точка №1: х:{first_point.x} у:{first_point.y}')
     second_point = Point(0, 5)
-    print(f'Точка №2: х:{second_point.x} у:{second_point.y}')
     third_point = Point(5, 0)
-    print(f'Точка №3: х:{third_point.x} у:{third_point.y}')
-
     triangle1 = Triangle(first_point, second_point, third_point)
+    print(
+        f'Створено трикутник №1 зі сторонами:\n1:{first_point.x},{first_point.y}\n2:{second_point.x},{second_point.y}\n3:{third_point.x},{third_point.y}')
     triangle1_area = triangle1.area()
-    print(f'Площа трикутника дорівнює {triangle1_area}')
     print(str(triangle1))
-
-    triangle2 = Triangle(Point(0,0),Point(0,5),Point(5,0))
+    first_point2 = Point(0, 0)
+    second_point2 = Point(0, 6)
+    third_point2 = Point(5, 0)
+    triangle2 = Triangle(first_point2, second_point2, third_point2)
+    print(
+        f'Створено трикутник №2 зі сторонами:\n1:{first_point2.x},{first_point2.y}\n2:{second_point2.x},{second_point2.y}\n3:{third_point2.x},{third_point2.y}')
     triangle2_area = triangle2.area()
-    print(triangle1 == triangle2)
+    print(f'Площа трикутника №1 дорівнює {triangle1_area}')
+    print(f'Площа трикутника №2 дорівнює {triangle2_area}')
+    print(f'Трикутник №1 дорівнює Трикутнику №2: {triangle1 == triangle2}')
+    print(f'Трикутник №1 не дорівнює Трикутнику №2: {triangle1 != triangle2}')
+    print(f'Трикутник №1 більший, аніж Трикутник №2: {triangle1 > triangle2}')
+    print(f'Трикутник №1 меньший, аніж Трикутник №2: {triangle1 < triangle2}')
+    print(f'Трикутник №1 більший або дорівнює Трикутнику №2: {triangle1 >= triangle2}')
+    print(f'Трикутник №1 меньший або дорівнює Трикутнику №2: {triangle1 <= triangle2}')
+
+
 if __name__ == "__main__":
     create_triangle()
